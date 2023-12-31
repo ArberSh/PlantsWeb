fc9206bd95d6f723019f255188658f20456b6862
Arber Shaska <arbershaska2@gmail.com>
2023-12-31 18:12:37 +0100

    Finished Promo

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -1,29 +1,44 @@
 import React from "react";
 import "../components/Promo.css"
+import Accessories from "../assests/Accessories.jpg"
+import GiftPlant from "../assests/GiftPlant.jpg"
+import NewArrivals from "../assests/NewArrivals.jpg"
+import Seeds from "../assests/Seeds.jpg"
+import Shipping from "../assests/Shipping.jpg"
 
 function Promo() {
   return (
     <div className="Promo">
       <div className="Promo_Container">
-        <div className="FirstPromo grid-col-span-2">
+        <div className="FirstPromo grid-col-span-2 ">
+          <div className="PromoText_Container">
           <h1>House Plant Accessories</h1>
           <button>Shop Accessories</button>
+          </div>
         </div>
-        <div className="SecondPromo">
-          <h1>Promotional Item</h1>
-          <button>Shop Gift Card</button>
-        </div>
-        <div className="ThirdPromo grid-col-span-2">
+        <div className="SecondPromo grid-col-span-4">
+        <div className="PromoText_Container">
           <h1>New Arrivals</h1>
           <button>Shop Now</button>
+          </div>
         </div>
-        <div className="ForthPromo">
-          <h1>Seed Packets</h1>
-          <button>Seed Seeds</button>
+        <div className="ThirdPromo grid-col-span-1 grid-row-1 forButton">
+        <div className="PromoText_Container">
+          <h1>Seeds Packet</h1>
+          <button>Shop Seeds</button>
+          </div>
         </div>
-        <div className="FifthPromo">
+        <div className="ForthPromo grid-col-span-1 grid-row-2 forButton">
+        <div className="PromoText_Container">
+          <h1>Promotional Item</h1>
+          <button>Shop Gift Card</button>
+          </div>
+        </div>
+        <div className="FifthPromo grid-col-span-1 grid-row-3 forButton">
+        <div className="PromoText_Container">
           <h1>Shipping Info</h1>
           <button>How we Ship Plants</button>
+          </div>
         </div>
       </div>
     </div>
