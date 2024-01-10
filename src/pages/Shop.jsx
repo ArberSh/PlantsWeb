37c9a27d5cfe5c278bdcfc69fb13bed1a5fb780a
Arber Shaska <arbershaska2@gmail.com>
2024-01-10 22:58:37 +0100

    Fixing radioButton

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,65 +5,25 @@ function Shop() {
   return (
     <div className='Shop'>
         <div className='Shop_Container'>
-        <div className='Filter_Container'>
-            <h1>Filter</h1>
-            <div className='Filter type'>
-                <h2>Type</h2>
-                <div>
-                <input type="radio" name="type" value="tree" />
-                <span className='square'></span>
-                <label>Tree</label>
-                </div>
-                <div>
-                <input type="radio" name="type" value="flowers" />
-                <span className='square'></span>
-                <label>Flowers</label>
-                </div>
-            </div>
-            <div className='Filter Caring'>
-            <h2>Caring</h2>
-            <div>
-            <input type="radio" name="care" value="easycare" />
-            <span className='square'></span>
-                <label>Easy-Care</label>
-                </div>
-                <div>
-                <input type="radio" name="care" value="mediumcare" />
-                <span className='square'></span>
-                <label>Medium-Care</label>
-                </div>
-            </div>
-            <div className='Filter SunLight'>
-                <h2>SunLight</h2>
-                <div>
-            <input type="radio" name="sunlight" value="full-sun" />
-            <span className='square'></span>
-                <label>Full Sun</label>
-                </div>
-                <div>
-                <input type="radio" name="sunlight" value="low-sun" />
-                <span className='square'></span>
-                <label>Low Sun</label>
-                </div>
-            </div>
-            <div className='Filter Watery'>
-                <h2>Watery</h2>
-                <div>
-                <input type="radio" name="watery" value="Frequent" />
-                <span className='square'></span>
-                <label>Low Sun</label>
-                </div>
-                <div>
-                <input type="radio" name="watery" value="Average" />
-                <span className='square'></span>
-                <label>Average</label>
-                </div>
-            </div>
-            
-        </div>
-        <div className='Plants_Container'>
+        {/* Example for the "Type" filter */}
+<div className='Filter type'>
+  <h2>Type</h2>
+  <div>
+    <input type="radio" id="tree" name="type" value="tree" />
+    <label htmlFor="tree">
+      <span className='square'></span>
+      Tree
+    </label>
+  </div>
+  <div>
+    <input type="radio" id="flowers" name="type" value="flowers" />
+    <label htmlFor="flowers">
+      <span className='square'></span>
+      Flowers
+    </label>
+  </div>
+</div>
 
-        </div>
         </div>
     </div>
   )
