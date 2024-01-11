f219d02510a630c383c753d01e6c8613885740c9
Arber Shaska <arbershaska2@gmail.com>
2024-01-11 23:33:01 +0100

    Fixing Api Error

--- src/components/Plants.jsx
+++ src/components/Plants.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../components/Plants.css'
 
 function Plants() {
   return (
     <div>
-        
+        <div className='Plant_Container '>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div className='Text_Container'>
+            <h1 className='price'>$15.99</h1>
+            <h4>Aloe Vera</h4>
+          </div>
+          <div>
+            <button>Quick Shop</button>
+          </div>
+        </div>
     </div>
   )
 }
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,29 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../pages/Shop.css'
+import Plants from '../components/Plants'
+import DATA from '../data.json'
+import axios from 'axios'
+
 
 function Shop() {
+
+    const [data,setData] = useState([])
+
+    useEffect(() => {
+    
+        async function fetchFood() {
+          try {
+            const { rensponse } = await axios.get(`https://perenual.com/api/species/details/1?key=sk-Om6T64c55486c6e241704`);
+            setData(rensponse.data);
+          } catch (error) {
+            console.log('Error', error);
+          }
+        }
+        fetchFood();
+      }
+      , [data]);
+    
+      useEffect(() => {
+        console.log(data);
+      }, [data]);
+            
+            
+
   return (
     <div className='Shop'>
         <div className='Shop_Container'>
-        {/* Example for the "Type" filter */}
-<div className='Filter type'>
-  <h2>Type</h2>
-  <div>
-    <input type="radio" id="tree" name="type" value="tree" />
-    <label htmlFor="tree">
-      <span className='square'></span>
-      Tree
-    </label>
-  </div>
-  <div>
-    <input type="radio" id="flowers" name="type" value="flowers" />
-    <label htmlFor="flowers">
-      <span className='square'></span>
-      Flowers
-    </label>
-  </div>
-</div>
-
+        <div className='Filter_Container'>
+            <h1>Filter</h1>
+            <div className='Filter type'>
+                <h2>Type</h2>
+                <div>
+                <input type="radio" name="type" value="tree" />
+                <span className='square'></span>
+                <label>Tree</label>
+                </div>
+                <div>
+                <input type="radio" name="type" value="flowers" />
+                <span className='square'></span>
+                <label>Flowers</label>
+                </div>
+            </div>
+            <div className='Filter Caring'>
+            <h2>Caring</h2>
+            <div>
+            <input type="radio" name="care" value="easycare" />
+            <span className='square'></span>
+                <label>Easy-Care</label>
+                </div>
+                <div>
+                <input type="radio" name="care" value="mediumcare" />
+                <span className='square'></span>
+                <label>Medium-Care</label>
+                </div>
+            </div>
+            <div className='Filter SunLight'>
+                <h2>SunLight</h2>
+                <div>
+            <input type="radio" name="sunlight" value="full-sun" />
+            <span className='square'></span>
+                <label>Full Sun</label>
+                </div>
+                <div>
+                <input type="radio" name="sunlight" value="low-sun" />
+                <span className='square'></span>
+                <label>Low Sun</label>
+                </div>
+            </div>
+            <div className='Filter Watery'>
+                <h2>Watery</h2>
+                <div>
+                <input type="radio" name="watery" value="Frequent" />
+                <span className='square'></span>
+                <label>Low Sun</label>
+                </div>
+                <div>
+                <input type="radio" name="watery" value="Average" />
+                <span className='square'></span>
+                <label>Average</label>
+                </div>
+            </div>
+            
+        </div>
+        <div className='Plants_Container'>
+            <div>
+                <Plants/>
+            </div>
+        </div>
         </div>
     </div>
   )
