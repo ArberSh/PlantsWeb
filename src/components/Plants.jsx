58912da0203a545f6e7d28ea2783ac4ae641adfd
Arber Shaska <arbershaska2@gmail.com>
2024-01-13 22:41:22 +0100

    Fixing some bugs

--- src/components/Plants.jsx
+++ src/components/Plants.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import '../components/Plants.css'
 
-function Plants() {
+function Plants({data}) {
+  
   return (
     <div>
         <div className='Plant_Container '>
           <div>
-            <img src="" alt="" />
+            <img src={data.thumbnail} alt="" />
           </div>
           <div className='Text_Container'>
             <h1 className='price'>$15.99</h1>
-            <h4>Aloe Vera</h4>
+            <h4>{data.common_name}</h4>
           </div>
           <div>
             <button>Quick Shop</button>
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,101 +1,97 @@
-import React, { useEffect, useState } from 'react'
-import '../pages/Shop.css'
-import Plants from '../components/Plants'
-import DATA from '../data.json'
-import axios from 'axios'
-
+import React, { useEffect, useState } from "react";
+import "../pages/Shop.css";
+import Plants from "../components/Plants";
+import DATA from "../data.json";
+import axios from "axios";
 
 function Shop() {
-
-    const [data,setData] = useState([])
-
-    useEffect(() => {
-    
-        async function fetchFood() {
-          try {
-            const { rensponse } = await axios.get(`https://perenual.com/api/species/details/1?key=sk-Om6T64c55486c6e241704`);
-            setData(rensponse.data);
-          } catch (error) {
-            console.log('Error', error);
-          }
-        }
-        fetchFood();
+  const [dataPlant, setData] = useState("");
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const { data } = await axios.get(
+          `https://perenual.com/api/species-list?key=sk-LPx165a266de2b29d3764`
+        );
+        setData(data.data);
+      } catch (error) {
+        console.log("Error", error);
       }
-      , [data]);
-    
-      useEffect(() => {
-        console.log(data);
-      }, [data]);
-            
-            
+    }
+    fetchPosts();
+  }, []);
+
+  useEffect(() => {
+    console.log(dataPlant);
+  }, [dataPlant]);
 
   return (
-    <div className='Shop'>
-        <div className='Shop_Container'>
-        <div className='Filter_Container'>
-            <h1>Filter</h1>
-            <div className='Filter type'>
-                <h2>Type</h2>
-                <div>
-                <input type="radio" name="type" value="tree" />
-                <span className='square'></span>
-                <label>Tree</label>
-                </div>
-                <div>
-                <input type="radio" name="type" value="flowers" />
-                <span className='square'></span>
-                <label>Flowers</label>
-                </div>
+    <div className="Shop">
+      <div className="Shop_Container">
+        <div className="Filter_Container">
+          <h1>Filter</h1>
+          <div className="Filter type">
+            <h2>Type</h2>
+            <div>
+              <input type="radio" name="type" value="tree" />
+              <span className="square"></span>
+              <label>Tree</label>
             </div>
-            <div className='Filter Caring'>
+            <div>
+              <input type="radio" name="type" value="flowers" />
+              <span className="square"></span>
+              <label>Flowers</label>
+            </div>
+          </div>
+          <div className="Filter Caring">
             <h2>Caring</h2>
             <div>
-            <input type="radio" name="care" value="easycare" />
-            <span className='square'></span>
-                <label>Easy-Care</label>
-                </div>
-                <div>
-                <input type="radio" name="care" value="mediumcare" />
-                <span className='square'></span>
-                <label>Medium-Care</label>
-                </div>
+              <input type="radio" name="care" value="easycare" />
+              <span className="square"></span>
+              <label>Easy-Care</label>
             </div>
-            <div className='Filter SunLight'>
-                <h2>SunLight</h2>
-                <div>
-            <input type="radio" name="sunlight" value="full-sun" />
-            <span className='square'></span>
-                <label>Full Sun</label>
-                </div>
-                <div>
-                <input type="radio" name="sunlight" value="low-sun" />
-                <span className='square'></span>
-                <label>Low Sun</label>
-                </div>
-            </div>
-            <div className='Filter Watery'>
-                <h2>Watery</h2>
-                <div>
-                <input type="radio" name="watery" value="Frequent" />
-                <span className='square'></span>
-                <label>Low Sun</label>
-                </div>
-                <div>
-                <input type="radio" name="watery" value="Average" />
-                <span className='square'></span>
-                <label>Average</label>
-                </div>
-            </div>
-            
-        </div>
-        <div className='Plants_Container'>
             <div>
-                <Plants/>
+              <input type="radio" name="care" value="mediumcare" />
+              <span className="square"></span>
+              <label>Medium-Care</label>
             </div>
+          </div>
+          <div className="Filter SunLight">
+            <h2>SunLight</h2>
+            <div>
+              <input type="radio" name="sunlight" value="full-sun" />
+              <span className="square"></span>
+              <label>Full Sun</label>
+            </div>
+            <div>
+              <input type="radio" name="sunlight" value="low-sun" />
+              <span className="square"></span>
+              <label>Low Sun</label>
+            </div>
+          </div>
+          <div className="Filter Watery">
+            <h2>Watery</h2>
+            <div>
+              <input type="radio" name="watery" value="Frequent" />
+              <span className="square"></span>
+              <label>Low Sun</label>
+            </div>
+            <div>
+              <input type="radio" name="watery" value="Average" />
+              <span className="square"></span>
+              <label>Average</label>
+            </div>
+          </div>
         </div>
+        <div className="Plants_Container">
+          <div>
+              {dataPlant.map((element) => (
+                <Plants key={element.id} data={element} />
+              ))}
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Shop
+export default Shop;
