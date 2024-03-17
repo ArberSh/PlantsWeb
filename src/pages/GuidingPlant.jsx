dac64e4ed7c3569c8bfd0fbd5fa9536eb79aff5a
Arber Shaska <arbershaska2@gmail.com>
2024-03-17 22:28:14 +0100

    Api is Fixed

--- src/pages/GuidingPlant.jsx
+++ src/pages/GuidingPlant.jsx
@@ -1,47 +1,91 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import '../pages/GuidingPlant.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "../pages/GuidingPlant.css";
 
 function GuidingPlant() {
-    const { id } = useParams();
-    const [plant, setPlant] = useState([]);
-    const [plant1, setPlant1] = useState([]);
+  const { id } = useParams();
+  const [plant, setPlant] = useState([]);
+  const [plantGuide, setPlantGuide] = useState([]);
+  const [plantGuide1, setPlantGuide1] = useState([]);
 
-    const NewId = id - 1;
+  const NewId = id - 1;
 
-    async function FetchPlant() {
-        const { data } = await axios.get(`https://perenual.com/api/species-care-guide-list?key=sk-LPx165a266de2b29d3764&page=2`);
-        setPlant(data.data);
-        console.log(plant)
-    }
-
-    async function FetchPlant1(species_id) {
-        const { data } = await axios.get(`https://perenual.com/api/species-care-guide-list?key=sk-LPx165a266de2b29d3764&page=1`);
-        setPlant1(data.data);
-        console.log(plant1)
-    }
-
-    useEffect(() => {
-        FetchPlant();
-    }, []);
-
-    useEffect(() => {
-        FetchPlant1(NewId);
-    }, []);
-
-    const filteredPlant = plant.find(elem => elem.species_id == id);
-    const filteredPlant1 = plant1.find(elem => elem.species_id == id);
-
-    return (
-        <div className='GuidingPlant'>
-            {filteredPlant1 || filteredPlant ? (
-                <h1>{filteredPlant.common_name}</h1>
-            ) : (
-                <p>Not Working</p> 
-            )}
-        </div>
+  async function FetchPlantGuide() {
+    const { data } = await axios.get(
+      `https://perenual.com/api/species-care-guide-list?key=sk-LPx165a266de2b29d3764&page=2`
     );
+    setPlantGuide(data.data);
+    console.log(plant);
+  }
+
+  async function FetchPlantGuide1() {
+    const { data } = await axios.get(
+      `https://perenual.com/api/species-care-guide-list?key=sk-LPx165a266de2b29d3764&page=1`
+    );
+    setPlantGuide1(data.data);
+  }
+
+  async function FetchPlant(id) {
+    const { data } = await axios.get(
+      `https://perenual.com/api/species-list?key=sk-qcAS65a265f29c4111704`
+    );
+    setPlant(data.data[id]);
+  }
+
+  useEffect(() => {
+    FetchPlantGuide();
+  }, []);
+
+  useEffect(() => {
+    FetchPlantGuide1();
+  }, []);
+
+  useEffect(() => {
+    FetchPlant(NewId);
+  }, []);
+
+  
+  const filteredPlant =
+  plantGuide.find((elem) => elem.species_id == id) ||
+  plantGuide1.find((elem) => elem.species_id == id);
+  
+//   console.log(filteredPlant.section[0].description)
+  return (
+    <div className="GuidingPlant">
+        <div className="GuidingPlantTitle">
+            {/* shife ktu lart */}
+      {filteredPlant ? (
+        <div>
+          <h1>HOW TO GUIDE</h1>
+          <h1 className="green1">{filteredPlant.common_name}</h1>
+          <img
+            src={
+              plant.default_image.thumbnail
+                ? plant.default_image.thumbnail
+                : plant.default_image.original_url
+            }
+            alt=""
+          />
+          <div className="DescriptionGuide">
+            <div className="Watering">
+                <div className="WateringImage">
+
+                </div>
+                <div className="WateringDescription">
+                    <h3>Watering:{plant.watering}</h3>
+                    <p>{filteredPlant.section[0].description}</p>
+                </div>
+            </div>
+          </div>
+          
+        </div>
+      ) : (
+        <p>Not Working</p>
+      )}
+      </div>
+    </div>
+  );
 }
 
 export default GuidingPlant;
