fd60e6dd8c78f7a1e0ae163d295466106d79cda3
Arber Shaska <arbershaska2@gmail.com>
2024-05-23 23:29:09 +0200

    Fixed a small bug in Cart

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import Nav from './pages/nav'
-import Home from './pages/Home.jsx'
-import './App.css';
-import Contact from './pages/Contact.jsx';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Shop from './pages/Shop.jsx';
-import PlantCare from './pages/PlantCare.jsx';
-import MoreInfo from './pages/MoreInfo.jsx';
-import GuidingPlant from './pages/GuidingPlant.jsx';
-import Cart from './pages/Cart.jsx';
+import Nav from './pages/nav';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Shop from './pages/Shop';
+import PlantCare from './pages/PlantCare';
+import MoreInfo from './pages/MoreInfo';
+import GuidingPlant from './pages/GuidingPlant';
+import Cart from './pages/Cart';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <>
-    <Nav/>
-    <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/shop" element={<Shop/>}/>
-    <Route path="/MoreInfo/:id" element={<MoreInfo />} />
-    <Route path="/PlantCare/" element={<PlantCare/>}/>
-    <Route path="/GuidePlant/:id" element={<GuidingPlant/>}/>
-    <Route path="/Cart" element={<Cart/>}/>
-    </Routes>
-    <Contact/>
-    </>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/MoreInfo/:id" element={<MoreInfo />} />
+          <Route path="/PlantCare" element={<PlantCare />} />
+          <Route path="/GuidePlant/:id" element={<GuidingPlant />} />
+          <Route path="/Cart" element={<Cart />} />
+        </Routes>
+        <Contact />
+      </>
     </Router>
   );
 }
